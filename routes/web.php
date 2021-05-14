0c30db8931cf9afd8c21a816af5727cad3b65b14
v_xxiaoji <v_xxiaoji@tencent.com>
2021-05-14 16:57:32 +0800

    larave/ui脚手架 php artisan ui:auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Auth\VerificationController;
 use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +20,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PagesController::class, 'root'])->name('root');
+
+// 用户身份验证相关的路由
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+// 用户注册相关路由
+Route::get('register', [RegisterController::class,'showRegistrationForm'])->name('register');
+Route::post('register', [RegisterController::class,'register']);
+// 密码重置相关路由
+Route::get('password/reset', [ForgotPasswordController::class,'showLinkRequestForm'])->name('password.request');
+Route::post('password/email', [ForgotPasswordController::class,'sendResetLinkEmail'])->name('password.email');
+Route::get('password/reset/{token}', [ResetPasswordController::class,'showResetForm'])->name('password.reset');
+Route::post('password/reset', [ResetPasswordController::class,'reset'])->name('password.update');
+// Email 认证相关路由
+Route::get('email/verify', [VerificationController::class,'show'])->name('verification.notice');
+Route::get('email/verify/{id}', [VerificationController::class,'verify'])->name('verification.verify');
+Route::get('email/resend', [VerificationController::class,'resend'])->name('verification.resend');
