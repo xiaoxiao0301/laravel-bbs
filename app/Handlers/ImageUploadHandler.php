51f3bf603e156dabc4da4c0bd252ed3fe0c75db5
v_xxiaoji <v_xxiaoji@tencent.com>
2021-05-17 15:43:45 +0800

    裁剪头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -5,6 +5,7 @@ namespace App\Handlers;
 
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 /**
  * 文件上传类
@@ -24,9 +25,10 @@ class ImageUploadHandler
      * @param UploadedFile $file
      * @param $folder
      * @param $file_prefix
+     * @param bool $max_width
      * @return array|boolean
      */
-    public function save($file, $folder, $file_prefix)
+    public function save($file, $folder, $file_prefix, $max_width = false)
     {
         // avatar目录存放头像 topic存放话题等，便于查找
         $folder_name = "upload/images/$folder/" . date("Ym/d", time());
@@ -43,12 +45,34 @@ class ImageUploadHandler
         // 移动图片到目标路径
         $file->move($upload_path, $filename);
 
+        // 根据图片宽度裁剪图片
+        if ($max_width && $extension != 'gif') {
+            $this->reduceSize($upload_path. '/' . $filename, $max_width);
+        }
+
         return [
             'path' => "/$folder_name/$filename"
         ];
     }
 
-
+    /**
+     * 裁剪图片
+     * @param $file_path
+     * @param $max_width
+     */
+    public function reduceSize($file_path, $max_width)
+    {
+        $image = Image::make($file_path);
+        // 进行大小调整的操作
+        $image->resize($max_width, null, function ($constraint) {
+            // 设定宽度是 $max_width，高度等比例双方缩放
+            $constraint->aspectRatio();
+            // 防止裁图时图片尺寸变大
+            $constraint->upsize();
+        });
+        // 对图片修改后进行保存
+        $image->save();
+    }
 
 
 }
