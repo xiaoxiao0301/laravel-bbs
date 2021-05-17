34655151e4f64ee58135c6b073164cbf9d41f1c6
v_xxiaoji <v_xxiaoji@tencent.com>
2021-05-17 11:37:56 +0800

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,24 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    /**
+     * 个人主页
+     * @param User $user
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
 
-
-    public function edit()
+    /**
+     * 编辑资料方法
+     * @param User $user
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function edit(User $user)
     {
-
+        return view('users.edit', compact('user'));
     }
 
-    public function update()
+    /**
+     * 修改资料
+     * @param UserRequest $request
+     * @param User $user
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(UserRequest $request, User $user)
     {
-
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功');
     }
 }
