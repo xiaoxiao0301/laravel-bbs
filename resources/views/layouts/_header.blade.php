9f72df2aca0e2cab42338e68aa2911e04d463a59
v_xxiaoji <v_xxiaoji@tencent.com>
2021-05-17 14:48:01 +0800

    显示头像

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -22,7 +22,7 @@
                 @else
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <img src="https://s2.ax1x.com/2019/11/21/M5K3pd.jpg" class="img-responsive img-circle" width="30px" height="30px">
+                                <img src="{{ $user->avatar }}" alt="{{ $user->name }}" class="img-responsive img-circle" width="30px" height="30px">
                                 {{ Auth::user()->name }}
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
