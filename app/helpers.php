174eb00c5ea34860acd0cd36f9a9b392720e966e
xxiaoji <jixiaoxiao0301@163.com>
2021-07-09 21:23:47 +0800

    heroku-postgresql

--- app/helpers.php
+++ app/helpers.php
@@ -15,3 +15,25 @@ function make_excerpt($body, $length = 200)
     $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($body)));
     return Str::limit($excerpt, $length);
 }
+
+function get_db_config()
+{
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
+        return $dbConfig = [
+            'connection' => 'pgsql',
+            'host' => $url["host"],
+            'database' => substr($url["path"], 1),
+            'username' => $url["user"],
+            'password' => $url["pass"]
+        ];
+    } else {
+        return $dbConfig = [
+            'connection' => env('DB_CONNECTION'),
+            'host' => env('DB_HOST'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', '')
+        ];
+    }
+}
--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$config = get_db_config();
+
 return [
 
     /*
@@ -66,11 +68,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $config['host'],
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $config['database'],
+            'username' => $config['username'],
+            'password' => $config['password'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
